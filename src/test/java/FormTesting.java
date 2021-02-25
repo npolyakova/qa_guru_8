710017e5d298e827a0fe1d2463ef8764e1ff85ab
Natalya <n.polyakova.kazanexpress@gmail.com>
2021-02-25 07:53:39 +0300

    added tests

---
+++ src/test/java/FormTesting.java
@@ -0,0 +1,82 @@
+import com.codeborne.selenide.Condition;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static utils.RandomUtils.*;
+
+public class FormTesting {
+
+    @BeforeAll
+    static void setup(){
+        open("https://demoqa.com/automation-practice-form");
+    }
+
+    @Test //Успешное заполнение формы
+    void formFillingWithCorrectData(){
+        Faker faker = new Faker();
+        String[] state = getRandomStateAndCity();
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String email = faker.internet().emailAddress();
+        String gender = getRandomGender();
+        String phone = getRandomPhone();
+        String year = getRandomInt(1900, 2100) + "";
+        String month = getRandomMonth() + "";
+
+        String day;
+        if (month.equals("February")){
+            day = getRandomInt(1, 28) + "";
+        }
+        else {
+            day = getRandomInt(1, 31) + "";
+        }
+
+        String hobby = getRandomHobby();
+        String address = faker.address().fullAddress();
+        String sub = getRandomSubject();
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $(byText(gender)).click();
+        $("#userNumber").setValue(phone);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__month").$(byText(day)).click();
+        $("#subjectsInput").setValue(sub).pressEnter();
+        $("#hobbiesWrapper").$(byText(hobby)).click();
+        $("#uploadPicture").uploadFile(new File("C:/Users/Natalya/Pictures/1588266690127712315.jpg"));
+        $("#currentAddress").setValue(address).scrollIntoView(true);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(state[0])).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(state[1])).click();
+
+        $("#submit").click();
+
+        $(".fade.modal.show").shouldBe(Condition.visible);
+        $(".modal-header").shouldHave(Condition.text("Thanks for submitting the form"));
+        $(".modal-body table thead").shouldHave(Condition.text("Label"), Condition.text("Values"));
+        $(".modal-body tbody tr", 0).shouldHave(Condition.text("Student Name"), Condition.text(firstName+" "+lastName));
+        $(".modal-body tbody tr", 1).shouldHave(Condition.text("Student Email"), Condition.text(email));
+        $(".modal-body tbody tr", 2).shouldHave(Condition.text("Gender"), Condition.text(gender));
+        $(".modal-body tbody tr", 3).shouldHave(Condition.text("Mobile"), Condition.text(phone));
+        $(".modal-body tbody tr", 4).shouldHave(Condition.text("Date of Birth"), Condition.text(day + " " + month + "," + year));
+        $(".modal-body tbody tr", 5).shouldHave(Condition.text("Subjects"), Condition.text(sub));
+        $(".modal-body tbody tr", 6).shouldHave(Condition.text("Hobbies"), Condition.text(hobby));
+        $(".modal-body tbody tr", 7).shouldHave(Condition.text("Picture"), Condition.text("1588266690127712315.jpg"));
+        $(".modal-body tbody tr", 8).shouldHave(Condition.text("Address"), Condition.text(address));
+        $(".modal-body tbody tr", 9).shouldHave(Condition.text("State and City"), Condition.text(state[0]+" "+state[1]));
+
+        $("#closeLargeModal").click();
+        $(".fade.modal.show").shouldNotBe(Condition.visible);
+
+    }
+}
---
+++ src/test/java/utils/RandomUtils.java
@@ -0,0 +1,88 @@
+package utils;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomUtils {
+
+    public static int getRandomInt(int min, int max) {
+        Random r = new Random();
+
+        return r.nextInt((max - min) + 1) + min;
+    }
+
+    public static Long getRandomLong(Long min, Long max) {
+        return ThreadLocalRandom.current().nextLong(min, max);
+    }
+
+    public static String getRandomPhone() {
+        return getRandomLong(1000000000L, 9999999999L)+"";
+    }
+
+    public static String getRandomGender() {
+        String[] genders = {"Male", "Female", "Other"};
+        int index = getRandomInt(0, genders.length - 1);
+
+        return genders[index];
+    }
+
+    public static String getRandomMonth() {
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August",
+                "September", "October", "November", "December"};
+        int index = getRandomInt(0, months.length - 1);
+
+        return months[index];
+    }
+
+    public static String getRandomHobby() {
+        String[] hobbies = {"Reading", "Sports", "Music"};
+        int index = getRandomInt(0, hobbies.length - 1);
+
+        return hobbies[index];
+    }
+
+    public static String getRandomSubject() {
+        String[] subjects = {"Maths", "Chemistry", "English", "Commerce", "Economics",
+                "Social Studies", "Computer Science", "Hindi", "Arts", "History",
+                "Accounting", "Physics", "Biology" };
+        int index = getRandomInt(0, subjects.length - 1);
+
+        return subjects[index];
+    }
+
+    public static String[] getRandomStateAndCity() {
+        String[] stateAndCity = new String[2];
+        String[] states = {"NCR", "Uttar Pradesh", "Haryana", "Rajasthan"};
+        String[] ncrCities = {"Delhi", "Gurgaon", "Noida"};
+        String[] upCities = {"Agra", "Lucknow", "Merrut"};
+        String[] harCities = {"Karnal", "Panipat"};
+        String[] rajCities = {"Jaipur", "Jaiselmer"};
+        int stateIndex = getRandomInt(0, states.length - 1);
+        int cityIndex;
+
+        switch (stateIndex) {
+            case (0) -> {
+                cityIndex = getRandomInt(0, ncrCities.length - 1);
+                stateAndCity[0]=states[stateIndex];
+                stateAndCity[1]=ncrCities[cityIndex];
+            }
+            case (1) -> {
+                cityIndex = getRandomInt(0, upCities.length - 1);
+                stateAndCity[0]=states[stateIndex];
+                stateAndCity[1]=upCities[cityIndex];
+            }
+            case (2) -> {
+                cityIndex = getRandomInt(0, harCities.length - 1);
+                stateAndCity[0]=states[stateIndex];
+                stateAndCity[1]=harCities[cityIndex];
+            }
+            case (3) -> {
+                cityIndex = getRandomInt(0, rajCities.length - 1);
+                stateAndCity[0]=states[stateIndex];
+                stateAndCity[1]=rajCities[cityIndex];
+            }
+        }
+
+        return stateAndCity;
+    }
+}
